cc070614934b75f8061825a64336711351e95e6f
Aron Springfield <aron@palringo.com>
2016-06-06 12:08:03 +0100

    Swapped m and h file the right way round

--- iOSUILib/iOSUILib/UIImageHelper.h
+++ iOSUILib/iOSUILib/UIImageHelper.h
@@ -1,39 +1,15 @@
 //
-//  UIImageHelper.m
+//  UIImageHelper.h
 //  Pods
 //
 //  Created by Aron Springfield on 06/06/2016.
 //
 //
 
-#import "UIImageHelper.h"
+#import <UIKit/UIKit.h>
 
-@implementation UIImage (MaterialControls)
+@interface UIImage (MaterialControls)
 
-- (UIImage *)imageWithTintColour:(UIColor *)colour
-{
-    // lets tint the icon - assumes your icons are solid colour
-    UIGraphicsBeginImageContextWithOptions(self.size, NO, [UIScreen mainScreen].scale);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    
-    CGContextTranslateCTM(context, 0, self.size.height);
-    CGContextScaleCTM(context, 1.0, -1.0);
-    
-    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
-    
-    // draw alpha-mask
-    CGContextSetBlendMode(context, kCGBlendModeNormal);
-    CGContextDrawImage(context, rect, self.CGImage);
-    
-    // draw tint color, preserving alpha values of original image
-    CGContextSetBlendMode(context, kCGBlendModeSourceIn);
-    [colour setFill];
-    CGContextFillRect(context, rect);
-    
-    UIImage *colouredImage = UIGraphicsGetImageFromCurrentImageContext();
-    UIGraphicsEndImageContext();
-    
-    return colouredImage;
-}
+- (UIImage *)imageWithTintColour:(UIColor *)color;
 
 @end
--- iOSUILib/iOSUILib/UIImageHelper.m
+++ iOSUILib/iOSUILib/UIImageHelper.m
@@ -1,15 +1,39 @@
 //
-//  UIImageHelper.h
+//  UIImageHelper.m
 //  Pods
 //
 //  Created by Aron Springfield on 06/06/2016.
 //
 //
 
-#import <UIKit/UIKit.h>
+#import "UIImageHelper.h"
 
-@interface UIImage (MaterialControls)
+@implementation UIImage (MaterialControls)
 
-- (UIImage *)imageWithTintColour:(UIColor *)color;
+- (UIImage *)imageWithTintColour:(UIColor *)colour
+{
+    // lets tint the icon - assumes your icons are solid colour
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, [UIScreen mainScreen].scale);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextTranslateCTM(context, 0, self.size.height);
+    CGContextScaleCTM(context, 1.0, -1.0);
+    
+    CGRect rect = CGRectMake(0, 0, self.size.width, self.size.height);
+    
+    // draw alpha-mask
+    CGContextSetBlendMode(context, kCGBlendModeNormal);
+    CGContextDrawImage(context, rect, self.CGImage);
+    
+    // draw tint color, preserving alpha values of original image
+    CGContextSetBlendMode(context, kCGBlendModeSourceIn);
+    [colour setFill];
+    CGContextFillRect(context, rect);
+    
+    UIImage *colouredImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return colouredImage;
+}
 
 @end
